3ff09bdcd7adac17a74384243df7d603fd2ca6c2
Dwayne <Dwayne@DWAYNE-PC>
2023-12-03 19:36:05 -0500

    Add project files.

---
+++ Params.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodParameters
+{
+    public class Params
+    {
+        //This method should return whatever String was passed into it by whatever invoked it (in this case, the two tests in Program.cs)
+        //to test the functionality of this method - it will always expect the method to return the same variable that was provided
+        //to the method.
+
+        public string copy(string text)
+        {
+            return text;        //"text" is a parameter passed into this method by whatever called it.
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using MethodParameters;     //Using the MethodParameters namespace to find the Params.copy method.
+using System;
+
+public class Program
+{
+    //The main method of this class is used to test the functionality of MethodParameters 'params' class.
+    
+    //create a main entry point for the application
+    public static void Main()
+    {
+        Console.WriteLine("The Params.copy method should return the same value was passed to it.");
+
+        //create a new instance/object of Params and name it "parrot"
+        Params parrot = new Params();
+
+        //create variables to test the Params copy method
+        Console.WriteLine("Here's what Params.copy when provided the value 'Repeat after me'");
+        string val1 = "Repeat after me";
+        string output1 = parrot.copy(val1);
+        Console.WriteLine(output1);
+
+        Console.WriteLine("Here's what Params.copy when provided the value 'Hello'");
+        string val2 = "Hello";
+        string output2 = parrot.copy(val2);
+        Console.WriteLine(output2);
+
+    }
+}
